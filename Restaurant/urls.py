2682f4f72c03fe349971bdf123cbac902dbcd0d2
tiagolch <tiagolch@hotmail.com>
2022-12-02 00:21:42 +0000

    add html
    Signed-off-by: tiagolch <tiagolch@hotmail.com>

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -1,17 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from core.views import index
 
 from core.api.viewsets import ComandViewset, ItemViewset, WaiterViewset
 
-routers = routers.DefaultRouter()
-
-routers.register(r'comand/', ComandViewset, basename='comand')
-routers.register(r'item/', ItemViewset, basename='item')
-routers.register(r'waiter/', WaiterViewset, basename='waiter')
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(routers.urls)),
+    path('', index, name='index')
 ]
 
