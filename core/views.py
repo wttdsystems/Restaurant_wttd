07919ed90edf9a88491f3691e8d3d23b71df3341
tiagolch <tiagolch@hotmail.com>
2022-12-02 00:21:22 +0000

    Co-authored-by: kelver-web <kelver-web@users.noreply.github.com> Co-authored-by: André Moreira Leopoldino <andremleopoldino@users.noreply.github.com> Co-authored-by: gcgava <gcgava@users.noreply.github.com>
    Signed-off-by: tiagolch <tiagolch@hotmail.com>

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from collections import defaultdict
 
-# Create your views here.
+tabs_values = defaultdict(int)
+def index(request):
+    if request.method == 'POST':
+        tab = request.POST.get('tab')
+        value = request.POST.get('value')
+        
+        tabs_values[tab] += int(value)
+        print(tabs_values)
+
+    return render(request, 'pedidos_form.html',{'total': tabs_values})
