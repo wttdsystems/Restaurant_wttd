1b82f5eb2db9735a291c0b865902f7e0f79c6d3e
Andre Moreira Leopoldino <andrem01310@gmail.com>
2022-12-06 20:59:11 -0300

    Arrumando estrutura do projeto

--- Restaurant/urls.py
+++ Restaurant/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework import routers
-from core.views import index
 
-from core.api.viewsets import ComandViewset, ItemViewset, WaiterViewset
+from core.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
