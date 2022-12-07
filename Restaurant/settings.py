0a99c4e787cae9b4702f53b4ec25747bedcaa208
Tiago Chaves <30913654+tiagolch@users.noreply.github.com>
2022-12-07 01:06:31 +0000

    sqlite

--- Restaurant/settings.py
+++ Restaurant/settings.py
@@ -86,14 +86,7 @@ WSGI_APPLICATION = 'Restaurant.wsgi.application'
 
 default_dburl = 'sqlite:///' + str(BASE_DIR)+'db.sqlite3'
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
-    }
+    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
 
 
