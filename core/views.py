b4585c21c071f757578964068df40f9171d368d1
tiagolch <tiagolch@hotmail.com>
2022-12-09 01:20:52 +0000

    Total de itens por comanda
    Co-authored-by: André Lucas <andrelucas07@users.noreply.github.com>
    Co-authored-by: André Moreira Leopoldino <andremleopoldino@users.noreply.github.com>
    Co-authored-by: Henrique C. C. de Andrade <henrique.ccda@gmail.com>
    Signed-off-by: tiagolch <tiagolch@hotmail.com>

--- core/views.py
+++ core/views.py
@@ -1,13 +1,41 @@
-from django.shortcuts import render
 from collections import defaultdict
 
-tabs_values = defaultdict(int)
+from django.shortcuts import render
+
+items = {
+    'coca': 10,
+    'pizza': 5,
+    "hamburguer": 15,
+}
+
+
+tabs_count = defaultdict(dict)
+tabs_values = {}
+
+
 def index(request):
+
     if request.method == 'POST':
         tab = request.POST.get('tab')
-        value = request.POST.get('value')
-        
-        tabs_values[tab] += int(value)
-        print(tabs_values)
+        item = request.POST.get('item')
 
-    return render(request, 'pedidos_form.html',{'total': tabs_values})
+        try:
+            tabs_count[tab][item] += 1
+        except KeyError:
+            tabs_count[tab][item] = 1
+
+        try:
+            tabs_values[tab] += items[item]
+        except KeyError:
+            tabs_values[tab] = items[item]
+
+        print(tabs_count, tabs_values)
+
+    return render(
+        request, 
+        'pedidos_form.html', 
+        {
+            'total': dict(tabs_values), 
+            'items': items
+        }
+    )
