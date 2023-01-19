7065ccbd89c0f0f639f4f93609add89be42bfba0
Andre Moreira Leopoldino <andrem01310@gmail.com>
2023-01-18 21:19:00 -0300

    Adding new template

--- core/tests/tests.py
+++ core/tests/tests.py
@@ -1,7 +1,7 @@
-from django.test import TestCase
 from django.shortcuts import resolve_url as r
+from django.test import TestCase
 
-from core.models import Item
+# from core.models import Item
 
 
 class TestIndex(TestCase):
@@ -12,19 +12,19 @@ class TestIndex(TestCase):
         self.assertEqual(200, self.response.status_code)
 
     def test_template(self):
-        self.assertTemplateUsed(self.response, 'pedidos_form.html')
+        self.assertTemplateUsed(self.response, 'index.html')
     
-    def test_form(self):
-        tags = (
-            ('<form', 1),
-            ('<input', 3),
-            ('type="text"', 2),
-            ('type="submit"', 1),
-            ('name="csrfmiddlewaretoken"', 1),
-            ('name="tab"', 1),
-            ('name="value"', 1),
+    # def test_form(self):
+    #     tags = (
+    #         ('<form', 1),
+    #         ('<input', 3),
+    #         ('type="text"', 2),
+    #         ('type="submit"', 1),
+    #         ('name="csrfmiddlewaretoken"', 1),
+    #         ('name="tab"', 1),
+    #         ('name="value"', 1),
 
-        )
-        for text, count in tags:
-            with self.subTest():
-                self.assertContains(self.response, text, count)
+    #     )
+        # for text, count in tags:
+        #     with self.subTest():
+        #         self.assertContains(self.response, text, count)
